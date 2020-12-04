ee5501df68930536d3141dbced2a5d80b82ec78b
Hèctor Morales <thehector1593@gmail.com>
2020-12-04 14:00:18 +0100

    He eliminat els preloads de la crida FindEventByID(); comprovar que no genera errors a altres crides

--- model/event/factory.go
+++ model/event/factory.go
@@ -47,7 +47,7 @@ func FindAll(ctx context.Context) (ctrl []Controller, err error) {
 	}
 
 	var eventsMOD []*Event
-	db.Preload(clause.Associations).Find(&eventsMOD)
+	db.Preload(clause.Associations).Preload("Services.Location").Preload("Services.Products").Find(&eventsMOD)
 
 	ctrl = make([]Controller, len(eventsMOD))
 	for index, event := range eventsMOD {
