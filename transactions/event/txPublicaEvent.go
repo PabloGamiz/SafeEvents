84e6a6b11ea9d9c624ab4c9efe958aca9f4209a4
Hèctor Morales <thehector1593@gmail.com>
2020-12-04 10:41:30 +0100

    testing: no merge

--- transactions/event/txPublicaEvent.go
+++ transactions/event/txPublicaEvent.go
@@ -5,61 +5,35 @@ import (
 	"log"
 
 	eventDTO "github.com/PabloGamiz/SafeEvents-Backend/dtos/event"
-	clientGW "github.com/PabloGamiz/SafeEvents-Backend/gateway/client"
-	"github.com/PabloGamiz/SafeEvents-Backend/model/client"
+	eventGW "github.com/PabloGamiz/SafeEvents-Backend/gateway/event"
 	eventMOD "github.com/PabloGamiz/SafeEvents-Backend/model/event"
-	"github.com/PabloGamiz/SafeEvents-Backend/model/session"
 )
 
 // txPublicaEvent represents an
 type txPublicaEvent struct {
-	request    eventDTO.DTO
-	sessCtrl   session.Controller
-	clientCtrl client.Controller
-	ctx        context.Context
-	event      *eventMOD.Event
+	request eventDTO.DTO
 }
 
-func (tx *txPublicaEvent) Precondition() (err error) { //Comprova que no existeix l'event
-	// make sure the session exists
-	tx.sessCtrl, err = session.GetSessionByID(tx.request.Cookie)
+func (tx *txPublicaEvent) Precondition() (err error) { //Comprova que no existeixi l'event
 	return
 }
 
-func (tx *txPublicaEvent) Postcondition(ctx context.Context) (v interface{}, err error) {
+func (tx *txPublicaEvent) Postcondition(ctx context.Context) (interface{}, error) {
 	log.Printf("Got a Publica Event request for event %s", tx.request.Title)
-	tx.event = &eventMOD.Event{
-		Title:       tx.request.Title,
-		Description: tx.request.Description,
-		Capacity:    tx.request.Capacity,
-		Price:       tx.request.Price,
-		CheckInDate: tx.request.CheckInDate,
-		ClosureDate: tx.request.ClosureDate,
-		Location:    tx.request.Location,
-		Image:       tx.request.Image,
-		Tipus:       tx.request.Tipus,
+
+	evnt := &eventMOD.Event{
+		Title: tx.request.Title,
 	}
+	gw := eventGW.NewEventGateway(ctx, evnt)
+	err := gw.Insert()
 
-	//eventGw := eventGW.NewEventGateway(ctx, tx.event)
-	//if err = eventGw.Insert(); err != nil {
-	//	return
-	//}
-
-	tx.sessCtrl.GetOrganizer().AddEvent(tx.event)
-	clientgw := clientGW.NewClientGateway(tx.ctx, tx.sessCtrl)
-	if err = clientgw.Update(); err != nil {
-		return
-	}
-
-	v = tx.event
-	return
+	return gw, err
 }
 
-func (tx *txPublicaEvent) Commit() (err error) {
-	return
+func (tx *txPublicaEvent) Commit() error {
+	return nil
 }
 
 func (tx *txPublicaEvent) Rollback() {
-	clientgw := clientGW.NewClientGateway(tx.ctx, tx.sessCtrl)
-	clientgw.Remove()
+
 }
