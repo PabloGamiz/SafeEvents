788ed2a5d4c31734d794a9f98f0941be7e529e8e
Carlos <carlos.plana.garcia@estudiantat.upc.edu>
2020-12-10 10:47:05 +0100

    cookie al body i falten tests

--- api/factory.go
+++ api/factory.go
@@ -25,7 +25,7 @@ func NewServer() Server {
 	// Events router Handlers
 	router.HandleFunc(event.APIListEvents, event.HandleListEventsRequest).Methods(http.MethodGet)
 	router.HandleFunc(event.APIPubliEvent, event.HandlePublicaEventRequest).Methods(http.MethodPost)
-	router.HandleFunc(event.APIListFavorites, event.HandleListFavoritesRequest).Methods(http.MethodGet)
+	router.HandleFunc(event.APIListFavorites, event.HandleListFavoritesRequest).Methods(http.MethodPut)
 	router.HandleFunc(event.APIGetEvent, event.HandleGetEventRequest).Methods(http.MethodPost)
 
 	// Ticket router Handlers
